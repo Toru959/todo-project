017ced6b86bd98088e1b80b72843b86fe3422ecb
Toru959 <80747436+Toru959@users.noreply.github.com>
2023-05-25 15:41:22 +0800

    navigation追加

--- resources/views/todo/index.blade.php
+++ resources/views/todo/index.blade.php
@@ -19,10 +19,10 @@
                             @foreach ($tasks as $task)
                             <div class="p-4 lg:w-1/2">
                               <div class="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
-                                <a href="{{ route('show',[$task->user->id]) }}"><img alt="team" class="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4" src="{{ ('https://dummyimage.com/200x200') }}"></a>
+                                <a href="{{ route('todo.show',$task->id) }}"><img alt="team" class="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4" src="{{ ('https://dummyimage.com/200x200') }}"></a>
                                 <div class="flex-grow sm:pl-8">
                                   <h2 class="title-font font-medium text-lg text-gray-900">{{ $task->title }}</h2>
-                                  <h3 class="text-gray-500 mb-3">{{ $task->user->name }}</h3>
+                                  <h3 class="text-gray-500 mb-3">{{ $task->id }}</h3>
                                   {{-- <p class="mb-4">DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.</p> --}}
                                   <span class="inline-flex">
                                     <a class="text-gray-500">
